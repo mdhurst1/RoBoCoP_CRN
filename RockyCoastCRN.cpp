79e5be080c47cfb4a5e208103af2ba4a7d119377
Martin Hurst <Martin.Hurst@glasgow.ac.uk>
2019-07-26 13:06:45 +0100

    added fastexp function

--- RockyCoastCRN.cpp
+++ RockyCoastCRN.cpp
@@ -76,6 +76,24 @@
 
 using namespace std;
 
+/* max. rel. error <= 1.73e-3 on [-87,88] */
+ float fastexp (float x)
+ {
+   volatile union {
+     float f;
+     unsigned int i;
+   } cvt;
+
+   /* exp(x) = 2^i * 2^f; i = floor (log2(e) * x), 0 <= f <= 1 */
+   float t = x * 1.442695041f;
+   float fi = floorf (t);
+   float f = t - fi;
+   int i = (int)fi;
+   cvt.f = (0.3371894346f * f + 0.657636276f) * f + 1.00172476f; /* compute 2^f */
+   cvt.i += (i << 23);                                          /* scale by 2^i */
+   return cvt.f;
+ }
+ 
 void RockyCoastCRN::Initialise()
 {
 	//cout << "Warning: You have not specified which nuclides you wish to track" << endl;
